53d7af098d6710e79dcfe794283da87d9d46521e
erfat <erfatjahan456@gmail.com>
2026-08-28 19:48:29 +0600

    fix:add samesite none and secure true in cookie

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -12,6 +12,8 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      secure: true,    
+      sameSite: "none",
     })
     .json({
       success: true,
