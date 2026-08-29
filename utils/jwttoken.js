a18255e8042f36d16413b43004243ae435c73c0c
erfat <erfatjahan456@gmail.com>
2026-08-29 10:30:36 +0600

    fix: jwt token and cookie configuration

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -1,23 +1,32 @@
 import jwt from "jsonwebtoken";
 
 export const sendToken = (user, statusCode, message, res) => {
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = jwt.sign(
+    { id: user.id }, 
+    process.env.JWT_SECRET_KEY, 
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN || "30d",
+    }
+  );
+
+  const expireDays = Number(process.env.COOKIE_EXPIRES_IN) || 30;
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + expireDays * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: true,     
+    sameSite: "none",
+  };
+
+  const userData = { ...user };
+  delete userData.password;
 
   res
     .status(statusCode)
-    .cookie("token", token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: true,    
-      sameSite: "none",
-    })
+    .cookie("token", token, cookieOptions)
     .json({
       success: true,
-      user,
+      user: userData,
       message,
       token,
     });
