61aadf792f3e32d413a72ea744765652f171bac2
erfat <erfatjahan456@gmail.com>
2026-09-05 12:44:06 +0600

    feat:ai search

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -465,7 +465,7 @@ export const fetchAIFilteredProducts = catchAsyncErrors(
       });
 
       filteredProducts = scoredProducts
-        .filter((p) => p.similarity >= 0.45)
+        .filter((p) => p.similarity >= 0.50)
         .sort((a, b) => b.similarity - a.similarity)
         .slice(0, 15);
     }
