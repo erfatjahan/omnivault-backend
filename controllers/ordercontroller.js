5ca4b9252c89e69d33077ead5c084c4e73f938a0
erfat <erfatjahan456@gmail.com>
2026-09-11 10:29:58 +0600

    fix:frontend url

--- controllers/ordercontroller.js
+++ controllers/ordercontroller.js
@@ -320,7 +320,7 @@ export const createPayForMeRequest = catchAsyncErrors(async (req, res, next) => 
     await client.query("COMMIT");
     client.release();
 
-    const frontendUrl = process.env.FRONTEND_URL || "https://omnivault-frontend-8o84ffcbo-erfatjahan.vercel.app";
+    const frontendUrl = process.env.FRONTEND_URL;
     const cleanFrontendUrl = frontendUrl.replace(/\/+$/, "");
     const payForMeUrl = `${cleanFrontendUrl}/pay-for-me/${paymentToken}`;
 
